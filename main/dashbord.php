7394225bc606bd6a6f078337aef56b0b8e2966fe
khomsunt <khom_meanmon@hotmail.com>
2023-04-20 21:47:36 +0700

    dashboard card

--- main/dashbord.php
+++ main/dashbord.php
@@ -1,35 +1,78 @@
-<div class="container-fluid col-12 p-3 my-3 bg-primary text-white">
+<div class="container-fluid col-12 p-3 bg-primary text-white">
     <h1>Dashboard</h1>
     <p>Demo</p>
-    <br><br><br>
+    <br><br><br><br><br>
 </div>
 
 <div class="row p-5" style="margin-top:-150px;">
-    <div class="col-lg-4 col-12 p-1">
-        <div class="container bg-warning rounded-3">Welcome to SB Admin Pro!
-            Browse our fully designed UI toolkit! Browse our prebuilt app pages, components, and utilites, and be sure
-            to look at our full documentation!</div>
+    <div class="col-xxl-4 col-12 p-1">
+        <div class="card shadow">
+            <div class="card-body">
+                Welcome to SB Admin Pro!
+                Browse our fully designed UI toolkit! Browse our prebuilt app pages, components, and utilites, and be
+                sure
+                to look at our full documentation!
+            </div>
+        </div>
     </div>
-    <div class="col-lg-4 col-12 p-1">
-        <div class="container bg-warning rounded-3">fsdf </div>
+    <div class="col-xxl-4 col-sm-6 col-12 p-1">
+        <div class="card shadow">
+            <div class="card-header">
+                Featured
+            </div>
+            <div class="card-body">
+                dddddddd
+            </div>
+        </div>
     </div>
-    <div class="col-lg-4 col-12 p-1">
-        <div class="container bg-warning rounded-3">fsdf</div>
+    <div class="col-xxl-4 col-sm-6 col-12 p-1">
+        <div class="card shadow">
+            <div class="card-header">
+                Featured
+            </div>
+            <div class="card-body">
+                dddddddd
+            </div>
+        </div>
     </div>
 </div>
 
 <div class="row p-5">
     <div class="col-lg-3 col-12 p-1">
-        <div class="container bg-warning rounded-3">fsdf </div>
+        <div class="card shadow bg-primary">
+            <div class="card-body">
+                fsdf
+            </div>
+        </div>
     </div>
     <div class="col-lg-3 col-12 p-1">
-        <div class="container bg-warning rounded-3">fsdf </div>
+        <div class="card shadow bg-success">
+            <div class="card-body">
+                fsdf
+            </div>
+        </div>
     </div>
     <div class="col-lg-3 col-12 p-1">
-        <div class="container bg-warning rounded-3">fsdf </div>
+        <div class="card shadow bg-warning">
+            <div class="card-body">
+                fsdf
+            </div>
+        </div>
     </div>
     <div class="col-lg-3 col-12 p-1">
-        <div class="container bg-warning rounded-3">fsdf </div>
+        <div class="card shadow bg-danger">
+            <div class="card-body">
+                fsdf
+                <br><br>
+                <br><br>
+            </div>
+            <div class="card-body border-top">
+                <div class="d-flex justify-content-between align-items-start">
+                    <span class="small">ddddddd</span>
+                    <span>></span>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 
--- template/default.php
+++ template/default.php
@@ -1,11 +1,9 @@
-<div class="container-fluid">
-    <div class="row">
-        <div id="topbar" class="col-12">
-        </div>
-    </div>
+<div class="container-fluid p-0">
     <div class="row" style="margin:0; padding:0;">
-        <div id="sidebar" class="col-sm-2 col-auto px-0 collapse show collapse-horizontal overflow-hidden" style="margin:0; padding:0;"></div>
-        <div id="display" class="col-sm col border-start ps-md-2 pt-2" style="margin:0; padding:0;"></div>
+        <div id="topbar" class="col-12 shadow"></div>
+        <div id="sidebar" class="col-sm-2 col-auto px-0 collapse show collapse-horizontal overflow-hidden"
+            style="margin:0; padding:0;"></div>
+        <div id="display" class="col-sm col border-start bg-light" style="margin:0; padding:0;"></div>
     </div>
 </div>
 
