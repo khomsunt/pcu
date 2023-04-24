34f76025f828d8d48bc7215f5daaa883f5a1457d
“inthada3124” <inthada3124@gmail.com>
2023-04-24 13:52:54 +0700

    update

--- map/sample.php
+++ map/sample.php
@@ -10,14 +10,35 @@
     <script>
     function myMap() {
         var mapProp = {
-            center: new google.maps.LatLng(51.508742, -0.120850),
-            zoom: 5,
+            center: new google.maps.LatLng(17.4151942494563, 103.78811052232832),
+            zoom: 9,
+            mapTypeId: google.maps.MapTypeId.HYBRID,
         };
         var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+
+        var mapMark = {
+            position: new google.maps.LatLng(17.4151942494563, 103.78811052232832),
+            map: map,
+
+        }
+        var marker = new google.maps.Marker(mapMark);
+        var infowindow = new google.maps.InfoWindow({
+            content: "Hello World!"
+        });
+        google.maps.event.addListener(marker, 'click', function() {
+            infowindow.open(map, marker);
+            var pos = map.getZoom();
+            map.setZoom(18);
+            map.setCenter(marker.getPosition());
+            window.setTimeout(function() {
+                map.setZoom(pos);
+            }, 3000);
+        });
     }
     </script>
 
-    <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY&callback=myMap"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBgrspFTB0_6fco5mPwD416MkStMgCXubE&callback=myMap">
+    </script>
 
 </body>
 
