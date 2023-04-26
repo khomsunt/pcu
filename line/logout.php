88f0d7cc7a80dddb1c534234eb3e8f1a879be9ca
khomsunt <khom_meanmon@hotmail.com>
2023-04-26 21:06:16 +0700

    login

---
+++ line/logout.php
@@ -0,0 +1,10 @@
+<?php
+require_once "../include/connection.php";
+require_once "../line/lineLoginLib.php";
+require_once "../line/lineConfig.php";
+unset(
+    $_SESSION['ses_login_accToken_val_' . $config['projectname']],
+    $_SESSION['ses_login_refreshToken_val_' . $config['projectname']],
+    $_SESSION['ses_login_userData_val_' . $config['projectname']],
+);
+$LineLogin->redirect('../main/index.php');
