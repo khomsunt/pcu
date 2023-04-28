bbd416f043521b2d6dd13830cb6e0a873776a9ef
khomsunt <khom_meanmon@hotmail.com>
2023-04-28 11:59:04 +0700

    Delete lineConfig.php

--- line/lineConfig.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('LINE_LOGIN_CHANNEL_ID', '1660984707');
-define('LINE_LOGIN_CHANNEL_SECRET', '65a4cd446970583aec3339936846a296');
-
-define('LINE_LOGIN_CALLBACK_URL', 'https://ce0e-61-19-108-218.ngrok-free.app/pcu/line/login_uselib_callback.php?projectName=pcu');
-
-$LineLogin = new LineLoginLib(
-    LINE_LOGIN_CHANNEL_ID, LINE_LOGIN_CHANNEL_SECRET, LINE_LOGIN_CALLBACK_URL);
