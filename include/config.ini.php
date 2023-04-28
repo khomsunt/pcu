2910cc0c171ef55cbfd06280c4ffb2794b027248
khomsunt <khom_meanmon@hotmail.com>
2023-04-28 12:05:05 +0700

    Delete config.ini.php

--- include/config.ini.php
+++
@@ -1,9 +0,0 @@
-<?php
-return array(
-    'hostname' => 'localhost',
-    'dbname' => 'pcu',
-    'username' => 'root',
-    'password' => '123456',
-    'port' => '3306',
-    'projectname' => 'pcu',
-);
