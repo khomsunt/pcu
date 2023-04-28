1b9d47e77212b06311b6f5cf9bd57bea4f74f48e
“inthada3124” <inthada3124@gmail.com>
2023-04-28 11:53:54 +0700

    ว

--- line/lineConfig.php
+++ line/lineConfig.php
@@ -2,6 +2,7 @@
 
 define('LINE_LOGIN_CHANNEL_ID', '1660984707');
 define('LINE_LOGIN_CHANNEL_SECRET', '65a4cd446970583aec3339936846a296');
+
 define('LINE_LOGIN_CALLBACK_URL', 'https://ce0e-61-19-108-218.ngrok-free.app/pcu/line/login_uselib_callback.php?projectName=pcu');
 
 $LineLogin = new LineLoginLib(
